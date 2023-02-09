879148f104bbec24cc75cd73ab24062bfd8ad83b
Craig W. Penner <cwpenner@gmail.com>
2023-02-09 15:34:25 -0700

    Removed call to creation_flags() inside group_spawn() to allow additional/alternate creation_flags to be specified before calling group_spawn().

--- src/stdlib/windows.rs
+++ src/stdlib/windows.rs
@@ -1,16 +1,14 @@
 use std::{
 	io::Result,
-	os::windows::{io::AsRawHandle, process::CommandExt},
+	os::windows::{io::AsRawHandle},
 	process::Command,
 };
-use winapi::um::winbase::CREATE_SUSPENDED;
 
 use crate::{winres::*, CommandGroup, GroupChild};
 
 impl CommandGroup for Command {
 	fn group_spawn(&mut self) -> Result<GroupChild> {
 		let (job, completion_port) = job_object()?;
-		self.creation_flags(CREATE_SUSPENDED);
 		let child = self.spawn()?;
 		assign_child(child.as_raw_handle(), job)?;
 
